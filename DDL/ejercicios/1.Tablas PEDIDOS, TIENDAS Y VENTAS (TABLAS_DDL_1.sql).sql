0ef50e752afa4006eeac90b1e1196fce3b3da352
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2025-03-19 12:12:24 +0100

    ejercicios

--- DDL/ejercicios/1.Tablas PEDIDOS, TIENDAS Y VENTAS (TABLAS_DDL_1.sql).sql
+++ DDL/ejercicios/1.Tablas PEDIDOS, TIENDAS Y VENTAS (TABLAS_DDL_1.sql).sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 -- 1. Modifica  las  columnas  de  la  tabla  PEDIDOS para  que  las  UNIDADES_PEDIDAS  puedan  almacenar cantidades numéricas de 6 dígitos. 
 
 select * from pedidos;
@@ -21,3 +22,28 @@ ALTER TABLE PEDIDOS ADD PVP NUMBER(4,0);
 ALTER TABLE VENTAS ADD PVP NUMBER(4,0);
 
 
+=======
+-- Tablas PEDIDOS, TIENDAS Y VENTAS (TABLAS_DDL_1.sql)
+select * from pedidos;
+select * from tiendas;
+select * from ventas;
+-- 1. Modifica las columnas de la tabla PEDIDOS para que las UNIDADES_PEDIDAS puedan almacenar
+-- cantidades numéricas de 6 dígitos.
+alter table pedidos
+modify unidades_pedidas number(6);
+-- 2. A partir de la tabla TIENDAS impide que se den de alta más tiendas en la provincia de 'TOLEDO'.
+alter table tiendas
+add constraint no_toledo check (poblacion != 'TOLEDO');
+-- 3. Añade a las tablas PEDIDOS y VENTAS una nueva columna para que almacenen el PVP del artículo.
+alter table pedidos
+add pvp_articulo decimal(6,2);
+alter table ventas
+add pvp_articulo decimal(6,2);
+
+-- Tablas EMP y DEPT (esquema SCOTT)
+desc scott.emp;
+desc scott.dept;
+
+select * from scott.emp;
+select * from scott.dept;
+>>>>>>> Stashed changes
--- DDL/ejercicios/2.Tablas EMP y DEPT (esquema SCOTT).sql
+++ DDL/ejercicios/2.Tablas EMP y DEPT (esquema SCOTT).sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 -- 4.  Crea una vista llamada NOMBRES con los campos ENAME, JOB, DEPTNO de la tabla EMP. 
 
 CREATE OR REPLACE VIEW Nombres --Creamos la vista con los campos que queremos mostrar
@@ -46,4 +47,57 @@ create or replace view NOMBRES4 as select emp.SAL from  Scott.emp;
 
 --10. Busca las vistas creadas en el diccionario de datos.
 
+=======
+-- 4. Crea una vista llamada NOMBRES con los campos ENAME, JOB, DEPTNO de la tabla EMP.
+create or replace view nombres
+as
+select ename, job, deptno from scott.emp;
+
+select * from user_views;
+
+-- 5. Crea una vista como la del punto anterior, pero que se llame NOMBRES2 y las
+-- columnas de la vista se llamen NOMBRE, PUESTO y DEPARTAMENTO.
+create or replace view nombres2 (Nombre,Puesto,Departamento)
+as
+select ename, job, deptno from scott.emp;
+
+select * from user_views;
+-- 6. Crea una vista llamada NOMBRES3, con los campos NOMBRE, PUESTO, DESC_DEPARTAMENTO y
+-- LOCALIZACION, pero solo de los empleados de Nueva York.
+create or replace view nombres3 (Nombre, Puesto, Desc_departamento, Localizacion)
+as
+select  emp.ename, emp.job, dept.dname, dept.loc
+from scott.emp, scott.dept
+where emp.deptno = dept.deptno
+and dept.loc ='NEW YORK';
+
+select * from user_views;
+-- 7. Modifica la tabla EMP, aumentando el tamaño de la columna ENAME a 30 caracteres.
+alter table scott.emp
+modify ename varchar2(30);
+
+-- 8. Utilizando la vista NOMBRES3, realiza las siguientes operaciones:
+-- - Inserta un nuevo analista llamado PEPE, con código 7000, en el departamento de Ventas.
+insert into nombres3 (empno, ename, job, deptno)
+values (7000, 'Pepe', 'ANALYST', (select deptno from scott.dept where dname = 'SALES'));
+
+-- - Modifica el nombre de los empleados, añadiéndoles el sufijo ‘(NY)’.
+update nombres3
+set ename = ename || ' (NY)';
+
+-- - Elimina a todos los empleados menos al presidente.
+delete from nombres3
+where job != 'PRESIDENT';
+
+-- 9. Crea una vista llamada NOMBRES4 con los totales de salario por departamento.
+create or replace view nombres4 (nombre_departamento, total_salarios)
+as
+select dname, sum(sal) "Total Salario" from scott.emp, scott.dept
+    where emp.deptno = dept.deptno
+    group by deptno, dname ;
+
+select * from user_views;
+
+-- 10. Busca las vistas creadas en el diccionario de datos.
+>>>>>>> Stashed changes
 select * from user_views;
